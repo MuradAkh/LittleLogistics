b15ae100db663a47848e5baec70255a575d674a0
Murad Akhundov <muradakhdev@gmail.com>
2021-12-04 20:01:11 -0500

    Mob Tug

--- src/main/java/dev/murad/shipping/entity/custom/TugEntity.java
+++ src/main/java/dev/murad/shipping/entity/custom/TugEntity.java
@@ -2,21 +2,60 @@ package dev.murad.shipping.entity.custom;
 
 import dev.murad.shipping.setup.ModEntityTypes;
 import dev.murad.shipping.setup.ModItems;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.LilyPadBlock;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.MoverType;
+import net.minecraft.entity.ai.attributes.AttributeModifierMap;
+import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.item.BoatEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.fluid.FluidState;
 import net.minecraft.item.Item;
+import net.minecraft.pathfinding.GroundPathNavigator;
 import net.minecraft.pathfinding.PathNavigator;
 import net.minecraft.pathfinding.SwimmerPathNavigator;
+import net.minecraft.tags.FluidTags;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.shapes.IBooleanFunction;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.util.math.vector.Vector3d;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
 
+import javax.annotation.Nullable;
+
 public class TugEntity extends MobEntity {
+
+    private float invFriction;
+    private float outOfControlTicks;
+    private float deltaRotation;
+    private int lerpSteps;
+    private double lerpX;
+    private double lerpY;
+    private double lerpZ;
+    private double lerpYRot;
+    private double lerpXRot;
+    private boolean inputLeft;
+    private boolean inputRight;
+    private boolean inputUp;
+    private boolean inputDown;
+    private double waterLevel;
+    private float landFriction;
+    private BoatEntity.Status status;
+    private BoatEntity.Status oldStatus;
+    private double lastYd;
+    private boolean isAboveBubbleColumn;
+    private boolean bubbleColumnDirectionIsDown;
+    private float bubbleMultiplier;
+    private float bubbleAngle;
+    private float bubbleAngleO;
 
     public TugEntity(EntityType<? extends MobEntity> type, World world) {
         super(type, world);
@@ -31,6 +70,236 @@ public class TugEntity extends MobEntity {
         this.yo = y;
         this.zo = z;
     }
+
+    public static AttributeModifierMap.MutableAttribute setCustomAttributes() {
+        return MobEntity.createMobAttributes()
+                .add(Attributes.MAX_HEALTH,1.0D)
+                .add(Attributes.MOVEMENT_SPEED,1.6D);
+
+    }
+
+    @Override
+    public boolean canBreatheUnderwater(){
+        return true;
+    }
+
+    @Override
+    protected PathNavigator createNavigation(World p_175447_1_) {
+        return new SwimmerPathNavigator(this, p_175447_1_);
+    }
+
+    @Override
+    public ActionResultType mobInteract(PlayerEntity player, Hand hand) {
+
+        navigation.moveTo(navigation.createPath(0, 63, 0, 1), 1);
+        return ActionResultType.PASS;
+    }
+
+    public void tick(){
+        this.oldStatus = this.status;
+        this.status = this.getStatus();
+
+        this.floatBoat();
+        this.move(MoverType.SELF, this.getDeltaMovement());
+
+
+
+        super.tick();
+    }
+
+    private void floatBoat() {
+        double d0 = (double)-0.04F;
+        double d1 = this.isNoGravity() ? 0.0D : (double)-0.04F;
+        double d2 = 0.0D;
+        this.invFriction = 0.05F;
+        if (this.oldStatus == BoatEntity.Status.IN_AIR && this.status != BoatEntity.Status.IN_AIR && this.status != BoatEntity.Status.ON_LAND) {
+            this.waterLevel = this.getY(1.0D);
+            this.setPos(this.getX(), (double)(this.getWaterLevelAbove() - this.getBbHeight()) + 0.101D, this.getZ());
+            this.setDeltaMovement(this.getDeltaMovement().multiply(1.0D, 0.0D, 1.0D));
+            this.lastYd = 0.0D;
+            this.status = BoatEntity.Status.IN_WATER;
+        } else {
+            if (this.status == BoatEntity.Status.IN_WATER) {
+                d2 = (this.waterLevel - this.getY()) / (double)this.getBbHeight();
+                this.invFriction = 0.9F;
+            } else if (this.status == BoatEntity.Status.UNDER_FLOWING_WATER) {
+                d1 = -7.0E-4D;
+                this.invFriction = 0.9F;
+            } else if (this.status == BoatEntity.Status.UNDER_WATER) {
+                d2 = (double)0.01F;
+                this.invFriction = 0.45F;
+            } else if (this.status == BoatEntity.Status.IN_AIR) {
+                this.invFriction = 0.9F;
+            } else if (this.status == BoatEntity.Status.ON_LAND) {
+                this.invFriction = this.landFriction;
+                if (this.getControllingPassenger() instanceof PlayerEntity) {
+                    this.landFriction /= 2.0F;
+                }
+            }
+
+            Vector3d vector3d = this.getDeltaMovement();
+            this.setDeltaMovement(vector3d.x * (double)this.invFriction, vector3d.y + d1, vector3d.z * (double)this.invFriction);
+            this.deltaRotation *= this.invFriction;
+            if (d2 > 0.0D) {
+                Vector3d vector3d1 = this.getDeltaMovement();
+                this.setDeltaMovement(vector3d1.x, (vector3d1.y + d2 * 0.06153846016296973D) * 0.75D, vector3d1.z);
+            }
+        }
+
+    }
+
+
+    public float getWaterLevelAbove() {
+        AxisAlignedBB axisalignedbb = this.getBoundingBox();
+        int i = MathHelper.floor(axisalignedbb.minX);
+        int j = MathHelper.ceil(axisalignedbb.maxX);
+        int k = MathHelper.floor(axisalignedbb.maxY);
+        int l = MathHelper.ceil(axisalignedbb.maxY - this.lastYd);
+        int i1 = MathHelper.floor(axisalignedbb.minZ);
+        int j1 = MathHelper.ceil(axisalignedbb.maxZ);
+        BlockPos.Mutable blockpos$mutable = new BlockPos.Mutable();
+
+        label39:
+        for(int k1 = k; k1 < l; ++k1) {
+            float f = 0.0F;
+
+            for(int l1 = i; l1 < j; ++l1) {
+                for(int i2 = i1; i2 < j1; ++i2) {
+                    blockpos$mutable.set(l1, k1, i2);
+                    FluidState fluidstate = this.level.getFluidState(blockpos$mutable);
+                    if (fluidstate.is(FluidTags.WATER)) {
+                        f = Math.max(f, fluidstate.getHeight(this.level, blockpos$mutable));
+                    }
+
+                    if (f >= 1.0F) {
+                        continue label39;
+                    }
+                }
+            }
+
+            if (f < 1.0F) {
+                return (float)blockpos$mutable.getY() + f;
+            }
+        }
+
+        return (float)(l + 1);
+    }
+
+    private BoatEntity.Status getStatus() {
+        BoatEntity.Status boatentity$status = this.isUnderwater();
+        if (boatentity$status != null) {
+            this.waterLevel = this.getBoundingBox().maxY;
+            return boatentity$status;
+        } else if (this.checkInWater()) {
+            return BoatEntity.Status.IN_WATER;
+        } else {
+            float f = this.getGroundFriction();
+            if (f > 0.0F) {
+                this.landFriction = f;
+                return BoatEntity.Status.ON_LAND;
+            } else {
+                return BoatEntity.Status.IN_AIR;
+            }
+        }
+    }
+
+    public float getGroundFriction() {
+        AxisAlignedBB axisalignedbb = this.getBoundingBox();
+        AxisAlignedBB axisalignedbb1 = new AxisAlignedBB(axisalignedbb.minX, axisalignedbb.minY - 0.001D, axisalignedbb.minZ, axisalignedbb.maxX, axisalignedbb.minY, axisalignedbb.maxZ);
+        int i = MathHelper.floor(axisalignedbb1.minX) - 1;
+        int j = MathHelper.ceil(axisalignedbb1.maxX) + 1;
+        int k = MathHelper.floor(axisalignedbb1.minY) - 1;
+        int l = MathHelper.ceil(axisalignedbb1.maxY) + 1;
+        int i1 = MathHelper.floor(axisalignedbb1.minZ) - 1;
+        int j1 = MathHelper.ceil(axisalignedbb1.maxZ) + 1;
+        VoxelShape voxelshape = VoxelShapes.create(axisalignedbb1);
+        float f = 0.0F;
+        int k1 = 0;
+        BlockPos.Mutable blockpos$mutable = new BlockPos.Mutable();
+
+        for(int l1 = i; l1 < j; ++l1) {
+            for(int i2 = i1; i2 < j1; ++i2) {
+                int j2 = (l1 != i && l1 != j - 1 ? 0 : 1) + (i2 != i1 && i2 != j1 - 1 ? 0 : 1);
+                if (j2 != 2) {
+                    for(int k2 = k; k2 < l; ++k2) {
+                        if (j2 <= 0 || k2 != k && k2 != l - 1) {
+                            blockpos$mutable.set(l1, k2, i2);
+                            BlockState blockstate = this.level.getBlockState(blockpos$mutable);
+                            if (!(blockstate.getBlock() instanceof LilyPadBlock) && VoxelShapes.joinIsNotEmpty(blockstate.getCollisionShape(this.level, blockpos$mutable).move((double)l1, (double)k2, (double)i2), voxelshape, IBooleanFunction.AND)) {
+                                f += blockstate.getSlipperiness(this.level, blockpos$mutable, this);
+                                ++k1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return f / (float)k1;
+    }
+
+    private boolean checkInWater() {
+        AxisAlignedBB axisalignedbb = this.getBoundingBox();
+        int i = MathHelper.floor(axisalignedbb.minX);
+        int j = MathHelper.ceil(axisalignedbb.maxX);
+        int k = MathHelper.floor(axisalignedbb.minY);
+        int l = MathHelper.ceil(axisalignedbb.minY + 0.001D);
+        int i1 = MathHelper.floor(axisalignedbb.minZ);
+        int j1 = MathHelper.ceil(axisalignedbb.maxZ);
+        boolean flag = false;
+        this.waterLevel = Double.MIN_VALUE;
+        BlockPos.Mutable blockpos$mutable = new BlockPos.Mutable();
+
+        for(int k1 = i; k1 < j; ++k1) {
+            for(int l1 = k; l1 < l; ++l1) {
+                for(int i2 = i1; i2 < j1; ++i2) {
+                    blockpos$mutable.set(k1, l1, i2);
+                    FluidState fluidstate = this.level.getFluidState(blockpos$mutable);
+                    if (fluidstate.is(FluidTags.WATER)) {
+                        float f = (float)l1 + fluidstate.getHeight(this.level, blockpos$mutable);
+                        this.waterLevel = Math.max((double)f, this.waterLevel);
+                        flag |= axisalignedbb.minY < (double)f;
+                    }
+                }
+            }
+        }
+
+        return flag;
+    }
+
+    @Nullable
+    private BoatEntity.Status isUnderwater() {
+        AxisAlignedBB axisalignedbb = this.getBoundingBox();
+        double d0 = axisalignedbb.maxY + 0.001D;
+        int i = MathHelper.floor(axisalignedbb.minX);
+        int j = MathHelper.ceil(axisalignedbb.maxX);
+        int k = MathHelper.floor(axisalignedbb.maxY);
+        int l = MathHelper.ceil(d0);
+        int i1 = MathHelper.floor(axisalignedbb.minZ);
+        int j1 = MathHelper.ceil(axisalignedbb.maxZ);
+        boolean flag = false;
+        BlockPos.Mutable blockpos$mutable = new BlockPos.Mutable();
+
+        for(int k1 = i; k1 < j; ++k1) {
+            for(int l1 = k; l1 < l; ++l1) {
+                for(int i2 = i1; i2 < j1; ++i2) {
+                    blockpos$mutable.set(k1, l1, i2);
+                    FluidState fluidstate = this.level.getFluidState(blockpos$mutable);
+                    if (fluidstate.is(FluidTags.WATER) && d0 < (double)((float)blockpos$mutable.getY() + fluidstate.getHeight(this.level, blockpos$mutable))) {
+                        if (!fluidstate.isSource()) {
+                            return BoatEntity.Status.UNDER_FLOWING_WATER;
+                        }
+
+                        flag = true;
+                    }
+                }
+            }
+        }
+
+        return flag ? BoatEntity.Status.UNDER_WATER : null;
+    }
+
+
 
 //    @Override
 //    public ActionResultType interact(PlayerEntity player, Hand hand) {
---
+++ src/main/java/dev/murad/shipping/setup/ModEventBusEvents.java
@@ -0,0 +1,16 @@
+package dev.murad.shipping.setup;
+
+import dev.murad.shipping.ShippingMod;
+import dev.murad.shipping.entity.custom.TugEntity;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = ShippingMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class ModEventBusEvents {
+    @SubscribeEvent
+    public static void addEntityAttributes(EntityAttributeCreationEvent event) {
+        event.put(ModEntityTypes.TUG.get(), TugEntity.setCustomAttributes().build());
+    }
+
+}
