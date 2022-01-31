9ade2ac130954b277a11f0f117da1896b79585c6
EDToaster <howard3061@gmail.com>
2022-01-30 23:00:04 -0800

    Removed IEnergyStorage from EnergyTugItem

--- src/main/java/dev/murad/shipping/item/EnergyTugItem.java
+++ src/main/java/dev/murad/shipping/item/EnergyTugItem.java
@@ -7,42 +7,12 @@ import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.energy.IEnergyStorage;
 
-public class EnergyTugItem extends AbstractEntityAddItem implements IEnergyStorage {
+public class EnergyTugItem extends AbstractEntityAddItem {
     public EnergyTugItem(Properties props) {
         super(props);
     }
 
     protected Entity getEntity(World world, RayTraceResult raytraceresult) {
         return new EnergyTugEntity(world, raytraceresult.getLocation().x, raytraceresult.getLocation().y, raytraceresult.getLocation().z);
-    }
-
-    @Override
-    public int receiveEnergy(int maxReceive, boolean simulate) {
-        return maxReceive;
-    }
-
-    @Override
-    public int extractEnergy(int maxExtract, boolean simulate) {
-        return maxExtract;
-    }
-
-    @Override
-    public int getEnergyStored() {
-        return 10;
-    }
-
-    @Override
-    public int getMaxEnergyStored() {
-        return 20;
-    }
-
-    @Override
-    public boolean canExtract() {
-        return true;
-    }
-
-    @Override
-    public boolean canReceive() {
-        return true;
     }
 }
