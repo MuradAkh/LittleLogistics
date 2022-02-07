c2a8fb96112231d5faefa85f3bbffa0f6e58a39f
Murad Akhundov <muradakhdev@gmail.com>
2022-02-06 20:57:18 -0500

    fix seater barge and bump config

--- src/main/java/dev/murad/shipping/entity/custom/SpringEntity.java
+++ src/main/java/dev/murad/shipping/entity/custom/SpringEntity.java
@@ -249,12 +249,12 @@ public class SpringEntity extends Entity implements IEntityAdditionalSpawnData {
             pos.set(compound.getInt("X"), compound.getInt("Y"), compound.getInt("Z"));
             String uuid = compound.getString("UUID");
             AxisAlignedBB searchBox = new AxisAlignedBB(
-                    pos.getX() - 5,
+                    pos.getX() - 2,
                     pos.getY() - 2,
-                    pos.getZ() - 5,
-                    pos.getX() + 5,
+                    pos.getZ() - 2,
+                    pos.getX() + 2,
                     pos.getY() + 2,
-                    pos.getZ() + 5
+                    pos.getZ() + 2
             );
             List<Entity> entities = level.getEntities(this, searchBox, e -> e.getStringUUID().equals(uuid));
             return entities.stream().findFirst().map(e -> (VesselEntity) e);
