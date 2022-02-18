53c65c6e20ba0bd23e2c4d01adbb836315d63686
EDToaster <howard3061@gmail.com>
2022-02-17 23:07:36 -0800

    Bump gradle and fix entity models

--- src/main/java/dev/murad/shipping/event/ModClientEventHandler.java
+++ src/main/java/dev/murad/shipping/event/ModClientEventHandler.java
@@ -2,6 +2,7 @@ package dev.murad.shipping.event;
 
 import dev.murad.shipping.ShippingMod;
 import dev.murad.shipping.block.fluid.render.FluidHopperTileEntityRenderer;
+import dev.murad.shipping.entity.models.*;
 import dev.murad.shipping.entity.render.ChestBargeRenderer;
 import dev.murad.shipping.entity.render.ChunkLoaderBargeRenderer;
 import dev.murad.shipping.entity.render.DummyEntityRenderer;
@@ -53,16 +54,32 @@ public class ModClientEventHandler {
     public static void onRegisterEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerEntityRenderer(ModEntityTypes.CHEST_BARGE.get(), ChestBargeRenderer::new);
         event.registerEntityRenderer(ModEntityTypes.CHUNK_LOADER_BARGE.get(), ChunkLoaderBargeRenderer::new);
+        event.registerEntityRenderer(ModEntityTypes.TUG_DUMMY_HITBOX.get(), DummyEntityRenderer::new);
+        event.registerEntityRenderer(ModEntityTypes.ENERGY_TUG.get(), EnergyTugRenderer::new);
         event.registerEntityRenderer(ModEntityTypes.FISHING_BARGE.get(), FishingBargeRenderer::new);
         event.registerEntityRenderer(ModEntityTypes.FLUID_TANK_BARGE.get(), FluidTankBargeRenderer::new);
         event.registerEntityRenderer(ModEntityTypes.SEATER_BARGE.get(), SeaterBargeRenderer::new);
-        event.registerEntityRenderer(ModEntityTypes.SPRING.get(), DummyEntityRenderer::new);
-        event.registerEntityRenderer(ModEntityTypes.TUG_DUMMY_HITBOX.get(), DummyEntityRenderer::new);
         event.registerEntityRenderer(ModEntityTypes.STEAM_TUG.get(), SteamTugRenderer::new);
-        event.registerEntityRenderer(ModEntityTypes.ENERGY_TUG.get(), EnergyTugRenderer::new);
-        event.registerEntityRenderer(ModEntityTypes.CHEST_BARGE.get(), ChestBargeRenderer::new);
+        event.registerEntityRenderer(ModEntityTypes.SPRING.get(), DummyEntityRenderer::new);
 
         event.registerBlockEntityRenderer(ModTileEntitiesTypes.FLUID_HOPPER.get(), FluidHopperTileEntityRenderer::new);
     }
+
+    @SubscribeEvent
+    public static void onRegisterEntityRenderers(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(ChainExtendedModel.LAYER_LOCATION, ChainExtendedModel::createBodyLayer);
+        event.registerLayerDefinition(ChainModel.LAYER_LOCATION, ChainModel::createBodyLayer);
+        event.registerLayerDefinition(ChestBargeModel.LAYER_LOCATION, ChestBargeModel::createBodyLayer);
+        event.registerLayerDefinition(ChunkLoaderBargeModel.LAYER_LOCATION, ChunkLoaderBargeModel::createBodyLayer);
+        event.registerLayerDefinition(EnergyTugModel.LAYER_LOCATION, EnergyTugModel::createBodyLayer);
+        event.registerLayerDefinition(FishingBargeDeployedModel.LAYER_LOCATION, FishingBargeDeployedModel::createBodyLayer);
+        event.registerLayerDefinition(FishingBargeModel.LAYER_LOCATION, FishingBargeModel::createBodyLayer);
+        event.registerLayerDefinition(FishingBargeTransitionModel.LAYER_LOCATION, FishingBargeTransitionModel::createBodyLayer);
+        event.registerLayerDefinition(FluidTankBargeModel.LAYER_LOCATION, FluidTankBargeModel::createBodyLayer);
+        event.registerLayerDefinition(SeaterBargeModel.LAYER_LOCATION, SeaterBargeModel::createBodyLayer);
+        event.registerLayerDefinition(SteamTugModel.LAYER_LOCATION, SteamTugModel::createBodyLayer);
+
+    }
+
 
 }
