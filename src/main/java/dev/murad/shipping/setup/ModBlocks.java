6ff49fdaea609356abf983c2e6caf34f0ba6e154
Murad Akhundov <muradakhdev@gmail.com>
2022-02-18 16:58:22 -0500

    Refactor linking (prep for locomotives)

--- src/main/java/dev/murad/shipping/setup/ModBlocks.java
+++ src/main/java/dev/murad/shipping/setup/ModBlocks.java
@@ -21,48 +21,49 @@ public class ModBlocks {
     public static final RegistryObject<Block> TUG_DOCK = register(
             "tug_dock",
             () -> new TugDockBlock(Block.Properties.of(Material.METAL)
+                    .destroyTime(0.5f)
             ),
             CreativeModeTab.TAB_TRANSPORTATION);
 
     public static final RegistryObject<Block> BARGE_DOCK = register(
             "barge_dock",
             () -> new BargeDockBlock(Block.Properties.of(Material.METAL)
-                    
+                    .destroyTime(0.5f)
             ),
             CreativeModeTab.TAB_TRANSPORTATION);
 
     public static final RegistryObject<Block> GUIDE_RAIL_CORNER = register(
             "guide_rail_corner",
             () -> new CornerGuideRailBlock(Block.Properties.of(Material.METAL)
-                    
+                    .destroyTime(0.5f)
             ),
             CreativeModeTab.TAB_TRANSPORTATION);
 
     public static final RegistryObject<Block> VESSEL_DETECTOR = register(
             "vessel_detector",
             () -> new VesselDetectorBlock(Block.Properties.of(Material.METAL)
-                    
+                    .destroyTime(0.5f)
             ),
             CreativeModeTab.TAB_TRANSPORTATION);
 
     public static final RegistryObject<Block> GUIDE_RAIL_TUG = register(
             "guide_rail_tug",
             () -> new TugGuideRailBlock(Block.Properties.of(Material.METAL)
-                    
+                    .destroyTime(0.5f)
             ),
             CreativeModeTab.TAB_TRANSPORTATION);
 
     public static final RegistryObject<Block> FLUID_HOPPER = register(
             "fluid_hopper",
             () -> new FluidHopperBlock(Block.Properties.of(Material.METAL)
-                    
+                    .destroyTime(0.5f)
             ),
             CreativeModeTab.TAB_TRANSPORTATION);
 
     public static final RegistryObject<Block> VESSEL_CHARGER = register(
             "vessel_charger",
             () -> new VesselChargerBlock(Block.Properties.of(Material.METAL)
-                    
+                    .destroyTime(0.5f)
             ),
             CreativeModeTab.TAB_TRANSPORTATION);
 
