bc850e68491ac0ab32b02c59e679c076c9850927
Murad Akhundov <muradakhdev@gmail.com>
2022-02-18 12:40:02 -0500

    fix leash render

--- src/main/java/dev/murad/shipping/entity/render/VesselRenderer.java
+++ src/main/java/dev/murad/shipping/entity/render/VesselRenderer.java
@@ -110,73 +110,57 @@ public abstract class VesselRenderer<T extends VesselEntity> extends EntityRende
     abstract EntityModel getModel(T entity);
 
 
-    private <E extends Entity> void renderLeash(T p_229118_1_, float p_229118_2_, PoseStack p_229118_3_, MultiBufferSource p_229118_4_, E p_229118_5_) {
-        p_229118_3_.pushPose();
-        Vec3 vector3d = p_229118_5_.getRopeHoldPosition(p_229118_2_);
-        double d0 = (double)(Mth.lerp(p_229118_2_, p_229118_1_.yBodyRot, p_229118_1_.yBodyRotO) * ((float)Math.PI / 180F)) + (Math.PI / 2D);
-        Vec3 vector3d1 = p_229118_1_.getLeashOffset();
-        double d1 = Math.cos(d0) * vector3d1.z + Math.sin(d0) * vector3d1.x;
-        double d2 = Math.sin(d0) * vector3d1.z - Math.cos(d0) * vector3d1.x;
-        double d3 = Mth.lerp(p_229118_2_, p_229118_1_.xo, p_229118_1_.getX()) + d1;
-        double d4 = Mth.lerp(p_229118_2_, p_229118_1_.yo, p_229118_1_.getY()) + vector3d1.y;
-        double d5 = Mth.lerp(p_229118_2_, p_229118_1_.zo, p_229118_1_.getZ()) + d2;
-        p_229118_3_.translate(d1, vector3d1.y, d2);
-        float f = (float)(vector3d.x - d3);
-        float f1 = (float)(vector3d.y - d4);
-        float f2 = (float)(vector3d.z - d5);
-        VertexConsumer ivertexbuilder = p_229118_4_.getBuffer(RenderType.leash());
-        Matrix4f matrix4f = p_229118_3_.last().pose();
+    private <E extends Entity> void renderLeash(T pEntityLiving, float pPartialTicks, PoseStack pMatrixStack, MultiBufferSource pBuffer, E pLeashHolder) {
+        pMatrixStack.pushPose();
+        Vec3 vec3 = pLeashHolder.getRopeHoldPosition(pPartialTicks);
+        double d0 = (double)(Mth.lerp(pPartialTicks, pEntityLiving.yBodyRot, pEntityLiving.yBodyRotO) * ((float)Math.PI / 180F)) + (Math.PI / 2D);
+        Vec3 vec31 = pEntityLiving.getLeashOffset();
+        double d1 = Math.cos(d0) * vec31.z + Math.sin(d0) * vec31.x;
+        double d2 = Math.sin(d0) * vec31.z - Math.cos(d0) * vec31.x;
+        double d3 = Mth.lerp((double)pPartialTicks, pEntityLiving.xo, pEntityLiving.getX()) + d1;
+        double d4 = Mth.lerp((double)pPartialTicks, pEntityLiving.yo, pEntityLiving.getY()) + vec31.y;
+        double d5 = Mth.lerp((double)pPartialTicks, pEntityLiving.zo, pEntityLiving.getZ()) + d2;
+        pMatrixStack.translate(d1, vec31.y, d2);
+        float f = (float)(vec3.x - d3);
+        float f1 = (float)(vec3.y - d4);
+        float f2 = (float)(vec3.z - d5);
+        float f3 = 0.025F;
+        VertexConsumer vertexconsumer = pBuffer.getBuffer(RenderType.leash());
+        Matrix4f matrix4f = pMatrixStack.last().pose();
         float f4 = Mth.fastInvSqrt(f * f + f2 * f2) * 0.025F / 2.0F;
         float f5 = f2 * f4;
         float f6 = f * f4;
-        BlockPos blockpos = new BlockPos(p_229118_1_.getEyePosition(p_229118_2_));
-        BlockPos blockpos1 = new BlockPos(p_229118_5_.getEyePosition(p_229118_2_));
-        int i = this.getBlockLightLevel(p_229118_1_, blockpos);
-        int k = p_229118_1_.level.getBrightness(LightLayer.SKY, blockpos);
-        int l = p_229118_1_.level.getBrightness(LightLayer.SKY, blockpos1);
-        renderSide(ivertexbuilder, matrix4f, f, f1, f2, i, i, k, l, 0.025F, 0.025F, f5, f6);
-        renderSide(ivertexbuilder, matrix4f, f, f1, f2, i, i, k, l, 0.025F, 0.0F, f5, f6);
-        p_229118_3_.popPose();
+        BlockPos blockpos = new BlockPos(pEntityLiving.getEyePosition(pPartialTicks));
+        BlockPos blockpos1 = new BlockPos(pLeashHolder.getEyePosition(pPartialTicks));
+        int i = this.getBlockLightLevel(pEntityLiving, blockpos);
+        int j = i;
+        int k = pEntityLiving.level.getBrightness(LightLayer.SKY, blockpos);
+        int l = pEntityLiving.level.getBrightness(LightLayer.SKY, blockpos1);
+
+        for(int i1 = 0; i1 <= 24; ++i1) {
+            addVertexPair(vertexconsumer, matrix4f, f, f1, f2, i, j, k, l, 0.025F, 0.025F, f5, f6, i1, false);
+        }
+
+        for(int j1 = 24; j1 >= 0; --j1) {
+            addVertexPair(vertexconsumer, matrix4f, f, f1, f2, i, j, k, l, 0.025F, 0.0F, f5, f6, j1, true);
+        }
+
+        pMatrixStack.popPose();
     }
 
-    /*
-        Shamelessly stolen from LuaX's fabric port of this mod
-     */
-    public static void renderSide(VertexConsumer p_229119_0_, Matrix4f p_229119_1_, float p_229119_2_, float p_229119_3_, float p_229119_4_, int p_229119_5_, int p_229119_6_, int p_229119_7_, int p_229119_8_, float p_229119_9_, float p_229119_10_, float p_229119_11_, float p_229119_12_) {
-        int i = 24;
-
-        for(int j = 0; j < 24; ++j) {
-            float f = (float)j / 23.0F;
-            int k = (int)Mth.lerp(f, (float)p_229119_5_, (float)p_229119_6_);
-            int l = (int)Mth.lerp(f, (float)p_229119_7_, (float)p_229119_8_);
-            int i1 = LightTexture.pack(k, l);
-            addVertexPair(p_229119_0_, p_229119_1_, i1, p_229119_2_, p_229119_3_, p_229119_4_, p_229119_9_, p_229119_10_, 24, j, false, p_229119_11_, p_229119_12_);
-            addVertexPair(p_229119_0_, p_229119_1_, i1, p_229119_2_, p_229119_3_, p_229119_4_, p_229119_9_, p_229119_10_, 24, j + 1, true, p_229119_11_, p_229119_12_);
-        }
-
-    }
-    public static void addVertexPair(VertexConsumer p_229120_0_, Matrix4f p_229120_1_, int lightUV, float p_229120_3_, float p_229120_4_, float p_229120_5_, float p_229120_6_, float p_229120_7_, int p_229120_8_, int p_229120_9_, boolean p_229120_10_, float p_229120_11_, float p_229120_12_) {
-        float f = 0.5F;
-        float f1 = 0.4F;
-        float f2 = 0.3F;
-        if (p_229120_9_ % 2 == 0) {
-            f *= 0.7F;
-            f1 *= 0.7F;
-            f2 *= 0.7F;
-        }
-
-        float f3 = (float)p_229120_9_ / (float)p_229120_8_;
-        float f4 = p_229120_3_ * f3;
-        float f5 = p_229120_4_ > 0.0F ? p_229120_4_ * f3 * f3 : p_229120_4_ - p_229120_4_ * (1.0F - f3) * (1.0F - f3);
-        float f6 = p_229120_5_ * f3;
-        if (!p_229120_10_) {
-            p_229120_0_.vertex(p_229120_1_, f4 + p_229120_11_, f5 + p_229120_6_ - p_229120_7_, f6 - p_229120_12_).color(f, f1, f2, 1.0F).uv2(lightUV).endVertex();
-        }
-
-        p_229120_0_.vertex(p_229120_1_, f4 - p_229120_11_, f5 + p_229120_7_, f6 + p_229120_12_).color(f, f1, f2, 1.0F).uv2(lightUV).endVertex();
-        if (p_229120_10_) {
-            p_229120_0_.vertex(p_229120_1_, f4 + p_229120_11_, f5 + p_229120_6_ - p_229120_7_, f6 - p_229120_12_).color(f, f1, f2, 1.0F).uv2(lightUV).endVertex();
-        }
-
+    private static void addVertexPair(VertexConsumer p_174308_, Matrix4f p_174309_, float p_174310_, float p_174311_, float p_174312_, int p_174313_, int p_174314_, int p_174315_, int p_174316_, float p_174317_, float p_174318_, float p_174319_, float p_174320_, int p_174321_, boolean p_174322_) {
+        float f = (float)p_174321_ / 24.0F;
+        int i = (int)Mth.lerp(f, (float)p_174313_, (float)p_174314_);
+        int j = (int)Mth.lerp(f, (float)p_174315_, (float)p_174316_);
+        int k = LightTexture.pack(i, j);
+        float f1 = p_174321_ % 2 == (p_174322_ ? 1 : 0) ? 0.7F : 1.0F;
+        float f2 = 0.5F * f1;
+        float f3 = 0.4F * f1;
+        float f4 = 0.3F * f1;
+        float f5 = p_174310_ * f;
+        float f6 = p_174311_ > 0.0F ? p_174311_ * f * f : p_174311_ - p_174311_ * (1.0F - f) * (1.0F - f);
+        float f7 = p_174312_ * f;
+        p_174308_.vertex(p_174309_, f5 - p_174319_, f6 + p_174318_, f7 + p_174320_).color(f2, f3, f4, 1.0F).uv2(k).endVertex();
+        p_174308_.vertex(p_174309_, f5 + p_174319_, f6 + p_174317_ - p_174318_, f7 - p_174320_).color(f2, f3, f4, 1.0F).uv2(k).endVertex();
     }
 }
