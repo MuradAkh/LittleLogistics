9a1f44abd0199706688d81361f2f842b2d702ea7
Murad Akhundov <muradakhdev@gmail.com>
2022-03-20 20:45:39 -0400

    stupid fix

--- src/main/java/dev/murad/shipping/entity/custom/tug/AbstractTugEntity.java
+++ src/main/java/dev/murad/shipping/entity/custom/tug/AbstractTugEntity.java
@@ -443,7 +443,7 @@ public abstract class AbstractTugEntity extends VesselEntity implements Linkable
 
 
     public void setPath(TugRoute path) {
-        if (!this.path.isEmpty()){
+        if (!this.path.isEmpty() && !this.path.equals(path)){
             this.nextStop = 0;
         }
         this.path = path;
