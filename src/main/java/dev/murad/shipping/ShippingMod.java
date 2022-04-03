dc15d70989d33bbe0179d487c70f8f36a512e039
Murad Akhundov <muradakhdev@gmail.com>
2022-04-03 18:12:03 -0400

    remove boilerplace from mod class

--- src/main/java/dev/murad/shipping/ShippingMod.java
+++ src/main/java/dev/murad/shipping/ShippingMod.java
@@ -37,12 +37,6 @@ public class ShippingMod
     public ShippingMod() {
         Registration.register();
 
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
         // Register the doClientStuff method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
@@ -53,12 +47,6 @@ public class ShippingMod
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void setup(final FMLCommonSetupEvent event)
-    {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-    }
-
     private void doClientStuff(final FMLClientSetupEvent event) {
         MenuScreens.register(ModMenuTypes.TUG_CONTAINER.get(), SteamTugScreen::new);
         MenuScreens.register(ModMenuTypes.ENERGY_TUG_CONTAINER.get(), EnergyTugScreen::new);
@@ -67,30 +55,5 @@ public class ShippingMod
         MenuScreens.register(ModMenuTypes.TUG_ROUTE_CONTAINER.get(), TugRouteScreen::new);
 
         event.enqueueWork(ModItemModelProperties::register);
-    }
-
-    private void enqueueIMC(final InterModEnqueueEvent event)
-    {
-        // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
-    }
-
-    private void processIMC(final InterModProcessEvent event)
-    {
-        // some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().
-                map(m->m.getMessageSupplier().get()).
-                collect(Collectors.toList()));
-    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
     }
 }
