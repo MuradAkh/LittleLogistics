e103e12859192ca64edb54ed4988b6f20014b5a6
Howard Chen 👾 <howard.chen@instacart.com>
2026-03-13 14:22:47 -0700

    fix(phase4): make CreativeEnergyStorage public so setup package can reference it

--- src/main/java/dev/murad/shipping/item/creative/CreativeCapacitor.java
+++ src/main/java/dev/murad/shipping/item/creative/CreativeCapacitor.java
@@ -2,13 +2,14 @@ package dev.murad.shipping.item.creative;
 
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Item.Properties;
+import net.minecraftforge.energy.IEnergyStorage;
 
 public class CreativeCapacitor extends Item {
     public CreativeCapacitor(Properties props) {
         super(props);
     }
 
-    static class CreativeEnergyStorage implements IEnergyStorage {
+    public static class CreativeEnergyStorage implements IEnergyStorage {
         @Override
         public int receiveEnergy(int maxReceive, boolean simulate) {
             return maxReceive;
