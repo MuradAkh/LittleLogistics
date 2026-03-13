597d1a1e242922574003adcd2eafa9f3a282290d
Howard Chen 👾 <howard.chen@instacart.com>
2026-03-13 15:30:14 -0700

    feat(phase7): migrate data gen infrastructure - LootTableProvider, DataGenerators, tag/model providers

--- src/main/java/dev/murad/shipping/data/ModBlockTagsProvider.java
+++ src/main/java/dev/murad/shipping/data/ModBlockTagsProvider.java
@@ -7,8 +7,8 @@ import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.TagsProvider;
 import net.minecraft.tags.BlockTags;
-import net.minecraftforge.common.data.BlockTagsProvider;
-import net.minecraftforge.common.data.ExistingFileHelper;
+import net.neoforged.neoforge.common.data.BlockTagsProvider;
+import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
