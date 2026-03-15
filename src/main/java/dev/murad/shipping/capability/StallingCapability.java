cfedb1c135620f3747fcdd9535769cdcbd251bf3
Howard Chen 👾 <howard.chen@instacart.com>
2026-03-14 21:31:05 -0700

    feat: add EntityCapability constant to StallingCapability interface

--- src/main/java/dev/murad/shipping/capability/StallingCapability.java
+++ src/main/java/dev/murad/shipping/capability/StallingCapability.java
@@ -1,6 +1,14 @@
 package dev.murad.shipping.capability;
 
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.capabilities.EntityCapability;
+
 public interface StallingCapability {
+    EntityCapability<StallingCapability, Void> ENTITY_CAPABILITY =
+        EntityCapability.createVoid(
+            ResourceLocation.fromNamespaceAndPath("littlelogistics", "stalling"),
+            StallingCapability.class);
+
     boolean isDocked();
     void dock(double x, double y, double z);
     void undock();
