ab3b98dc3c528371fab821020f3a809048d32a47
EDToaster <howard3061@gmail.com>
2026-07-18 23:27:16 -0700

    fix: guard steam locomotive fuel drop against chunk unload to prevent item duplication

--- src/main/java/dev/murad/shipping/entity/custom/train/locomotive/SteamLocomotiveEntity.java
+++ src/main/java/dev/murad/shipping/entity/custom/train/locomotive/SteamLocomotiveEntity.java
@@ -107,7 +107,7 @@ public class SteamLocomotiveEntity extends AbstractLocomotiveEntity implements I
 
     @Override
     public void remove(RemovalReason r) {
-        if(!this.level().isClientSide){
+        if(!this.level().isClientSide && r != RemovalReason.UNLOADED_TO_CHUNK){
             Containers.dropContents(this.level(), this, this);
         }
         super.remove(r);
